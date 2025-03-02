d33c23b800b6e8055d79179704adc2343d40218d
myouse652 <mahdi_yousefibaviloliaei@comcast.com>
2025-03-01 19:36:54 -0800

    Initial commit for PlayWrite project

--- tests/Day 2/LocatingMultipleElements.spec.js
+++ tests/Day 2/LocatingMultipleElements.spec.js
@@ -4,22 +4,10 @@ test('LocateMultipleElements', async ({ page }) => {
 
 await page.goto('https://demoblaze.com/')
 
-// Fing all the links on the page
-// const links = await page.$$('a');
-
-// for (const each of  links){
-
-// const linkText =   await each.textContent();
-// console.log(linkText)
-
-// }
-
-// 
-
     // Wait for the presence of the product links using XPath
     await page.waitForSelector('xpath=//*[@id="tbodyid"]//div');
   
-    // Locate all product links using XPath
+    // Locate all product links using XPath - look for web elements
     const products = await page.$$('xpath=//*[@id="tbodyid"]//div/h4/a');
   
     // Loop through the product links and print each product's name
---
+++ tests/Day3/Locators_builtin.spec.js
@@ -0,0 +1,45 @@
+
+/*
+
+
+page.getByLabel() to locate a form control by associated label's text.
+page.getByTitle() to locate an element by its title attribute.
+page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
+*/
+
+
+
+const { test, expect } = require('@playwright/test');
+
+test('Built-inLocators', async ({ page }) => {
+
+
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+
+//page.getByAltText() to locate an element, usually image, by its text alternative.
+const logo = await page.getByAltText('company-brandin')
+await expect(logo).toBeVisible()
+
+// await expect(page.getByAltText('company-branding')).toBeVisible()
+
+
+//page.getByPlaceholder() to locate an input by placeholder.
+await page.getByPlaceholder('Username').fill("Admin")
+await page.getByPlaceholder('Password').fill("admin123")
+
+
+//page.getByRole() to locate by explicit and implicit accessibility attributes.
+await page.getByRole('button',{type:'submit'}).click()
+
+
+//page.getByText() to locate by text content.
+const name = await page.locator("//p[@class = 'oxd-userdropdown-name']").textContent()
+await expect(await page.getByText(name)).toBeVisible()
+
+
+
+await page.close()
+
+
+})
